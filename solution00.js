0519ade163ae6de15b5d6858f57fab0a7759d9a5
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-05-26 16:39:29 -0300

    codewars practice of the day

---
+++ solution00.js
@@ -0,0 +1,11 @@
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+function nextId(ids){
+    for (let i = 0; i <= ids.length; i++){
+      if(!ids.includes(i)){
+        return i
+      }
+    }
+  }
