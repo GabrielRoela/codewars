da2c55b3c555c422a9a1dbdfbac11b5745c7a0ed
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-05-29 15:07:17 -0300

    codewars practice

---
+++ solution06.js
@@ -0,0 +1,7 @@
+// create a function that swaps values IN PLACE in a 2 value array 
+
+function swapValues(arguments) {
+    let hold = arguments[0]
+    arguments[0] = arguments[1]
+    arguments[1] = hold
+  }
