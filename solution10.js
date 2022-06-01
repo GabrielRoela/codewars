bd58375a6d2766c979828512e74d4685bc53908c
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-06-01 15:47:40 -0300

    codewars practice

---
+++ solution10.js
@@ -0,0 +1,17 @@
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+function getDivisorsCnt(n){
+    let counter = 0
+    for(let i = n; i > 0; i--){
+      n % i == 0 ? counter++ : 'we'
+    }
+  return counter
+}
