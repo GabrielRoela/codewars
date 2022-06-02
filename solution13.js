3ba601db304f680cd3c64ef2f8d8dc3d218d187c
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-06-02 11:05:00 -0300

    codewars practice

---
+++ solution13.js
@@ -0,0 +1,12 @@
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+    return array.map((e,i) => `${1 + i}: ${e}`)
+  }
