8db79f4fa1bc8c8f5a2887507f880c18d8c972c1
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-06-21 17:42:48 -0300

    codewars practice

---
+++ solution34.js
@@ -0,0 +1,8 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+function odds(values){
+    return values.filter(e => e % 2 != 0)
+  }
