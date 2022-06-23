c7760eefc30fe8768402b30f6ddf54b7de4914fe
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-06-23 19:40:00 -0300

    codewars practice

---
+++ solution36.js
@@ -0,0 +1,15 @@
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+function cleanString(s) {
+    let processed = []
+    s.split('').forEach(e => e === '#' ? processed.pop() : processed.push(e))
+    return processed.join('')
+  }
