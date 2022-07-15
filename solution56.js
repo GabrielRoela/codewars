90aa04b99ed85dda3da8ee46b8b640345beae91a
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-07-15 19:32:50 -0300

    codewars practice

---
+++ solution56.js
@@ -0,0 +1,23 @@
+// Description:
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+function remove(s,n){
+    for(let i = 1; i <= n; i++){
+      s = s.replace('!','')
+    }
+    
+    return s
+  }
