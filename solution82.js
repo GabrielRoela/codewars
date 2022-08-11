f8d3ad11d1075219292760df9f61b7ec9f091a68
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-08-11 11:53:52 -0300

    codewars practice

---
+++ solution82.js
@@ -0,0 +1,132 @@
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+// It is much easier to understand with an example:
+
+// * For seconds = 62, your function should return 
+//     "1 minute and 2 seconds"
+// * For seconds = 3662, your function should return
+//     "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// Note that spaces are important.
+
+// Detailed rules
+// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+
+function formatDuration (seconds) {
+
+    if(!seconds){
+      return 'now'
+    }
+  
+    let years = Math.floor(seconds / 31536000)
+    let days = Math.floor(seconds % 31536000 / 86400)
+    let hours = Math.floor(seconds % 31536000 % 86400 / 3600)
+    let minutes = Math.floor(seconds % 31536000 % 86400 % 3600 / 60)
+    let secondss = Math.floor(seconds % 31536000 % 86400 % 3600 % 60)
+    let humanReadable = ''
+  
+    if(years > 1){
+      humanReadable+= `${years} years`
+    }
+    
+    if(years == 1){
+      humanReadable+= `${years} year`
+    }
+  
+    if(days > 1){
+      if(humanReadable){
+        humanReadable+= `, ${days} days`
+      }else{
+        humanReadable+= `${days} days`
+      }
+    }
+  
+    if(days == 1){
+      if(humanReadable){
+        humanReadable+= `, ${days} day`
+      }else{
+        humanReadable+= `${days} day`
+      }
+    }
+  
+    if(hours > 1){
+      if(humanReadable){
+        humanReadable+= `, ${hours} hours`
+      }else{
+        humanReadable+= `${hours} hours`
+      }
+    }
+  
+    if(hours == 1){
+      if(humanReadable){
+        humanReadable+= `, ${hours} hour`
+      }else{
+        humanReadable+= `${hours} hour`
+      }
+    }
+  
+    if(minutes > 1){
+      if(secondss){
+        if(humanReadable){
+          humanReadable+= `, ${minutes} minutes`
+        }else{
+          humanReadable+= `${minutes} minutes`
+        }
+      }else{
+        if(humanReadable){
+          humanReadable+= ` and ${minutes} minutes`
+        }else{
+          humanReadable+= `${minutes} minutes`
+        }
+      }
+    }
+  
+    if(minutes == 1){
+      if(secondss){
+        if(humanReadable){
+          humanReadable+= `, ${minutes} minute`
+        }else{
+          humanReadable+= `${minutes} minute`
+        }
+      }else{
+        if(humanReadable){
+          humanReadable+= ` and ${minutes} minute`
+        }else{
+          humanReadable+= `${minutes} minute`
+        }
+      }
+    }
+  
+    if(secondss > 1){
+      if(humanReadable){
+        humanReadable+= ` and ${secondss} seconds`
+      }else{
+        humanReadable+= `${secondss} seconds`
+      }
+    }
+  
+    if(secondss == 1){
+      if(humanReadable){
+        humanReadable+= ` and ${secondss} second`
+      }else{
+        humanReadable+= `${secondss} second`
+      }
+    }
+  
+    return humanReadable
+  
+  }
+  
