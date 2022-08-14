8fddad4d3301c86d89c8023964a60ce2c0a4948a
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-08-14 14:07:31 -0300

    codewars practice

---
+++ solution85.js
@@ -0,0 +1,25 @@
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n){
+    let counter = 0.00
+    let adding = 1
+      for(let i = 1; i <= n; i++){
+        counter += 1 / adding
+        adding+=3
+      }
+    return String(counter.toFixed(2))
+  }
