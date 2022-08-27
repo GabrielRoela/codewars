5a274801012da1c4e18cc7571a187a756a30405e
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-08-27 15:40:32 -0300

    coding challenge

---
+++ solution97.js
@@ -0,0 +1,14 @@
+// Challenge Instructions: Construct a function multiMap that will accept two arrays: an array of values and an array of callbacks. multiMap will return an object whose keys match the elements in the array of values. The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key.
+
+// Example:
+// console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
+// should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+function multiMap(arr1, arr2){
+    let map = {}
+    arr1.forEach(e => map[e] = [])
+    arr1.forEach(e => arr2.forEach(func => map[e].push(func(e))))
+    return map
+}
+
+console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
