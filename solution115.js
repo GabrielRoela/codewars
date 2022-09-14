db17562c9a63e999b59c0a2cc8910dea7f3ca9c3
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-09-14 13:30:00 -0300

    codewars practice

---
+++ solution115.js
@@ -0,0 +1,18 @@
+// test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+Array.prototype.square = function() {return this.map(e => e*e)}
+Array.prototype.cube = function() {return this.map(e => e**3)}
+Array.prototype.average = function() {return this.reduce((pv, cv) => pv + cv, 0) / this.length}
+Array.prototype.sum = function() {return this.reduce((pv, cv) => pv + cv, 0)}
+Array.prototype.even = function() {return this.filter(e => e % 2 == 0)}
+Array.prototype.odd = function() {return this.filter(e => e % 2 !== 0)}
