020a424d2a91a8b1274177aaf5a1a147733a3da5
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-10-18 20:28:14 -0300

    codewars practice

---
+++ solution146.js
@@ -0,0 +1,5 @@
+// Mark an object as frozen so that no other changes can be made to it.
+
+Object.freeze(Obj)
+
+//Obj.freeze makes so this object CANNOT BE CHANGED ANYMORE. methods and properties included
