3d52a7114bf7cccd564845fe8ce57ee437aab553
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-10-24 15:22:24 -0300

    codewars practice

---
+++ solution152.js
@@ -0,0 +1,13 @@
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
+
+function digitize(n) {
+    return Array.from(n.toString()).map(e => Number(e))
+  }
