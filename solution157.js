fc917e02c3d6046a2eec79e567e069bbb0048af5
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-10-29 15:00:00 -0300

    codewars practice

---
+++ solution157.js
@@ -0,0 +1,10 @@
+// Your task is to write function factorial.
+
+function factorial(n){
+    let result = 1
+    for(let i = 2; i <= n; i++){
+      result = result * i
+    }
+    
+    return result
+  }
