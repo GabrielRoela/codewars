842fa656197b6f5fcf7d696923789db9031dbfa9
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-11-14 16:30:11 -0300

    codewars practice

---
+++ solution173.js
@@ -0,0 +1,18 @@
+// ou need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) {
+    let minAndMax = [Math.min(...triplet),Math.max(...triplet)]
+    return triplet.indexOf( ...triplet.filter(e => !minAndMax.includes(e)) )
+  }
