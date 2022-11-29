e7e98f3c7b04a4ad95c1c2affa1b2063357c171f
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-11-29 07:57:05 -0300

    codewars practice

---
+++ solution188.js
@@ -0,0 +1,15 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces 
+
+// dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+// dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+// dup(["kelless","keenness"]), ['keles','kenes'])
+
+function dup(arr){
+    return arr.map(e => e.split('').filter((e,i,arr) => e !== arr[i-1]).join(''))
+}
