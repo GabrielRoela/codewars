d77fb58aecccf2a41b0faba6407947ab0ee74559
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-12-09 16:02:28 -0300

    leetcode practice

---
+++ solution198.js
@@ -0,0 +1,44 @@
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+ 
+
+// Example 1:
+
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// Example 2:
+
+// Input: s = "a"
+// Output: 1
+// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2000
+// s consists of lowercase and/or uppercase English letters only.
+
+var longestPalindrome = function(s) {
+    let charCount = s.split('').reduce((counter,char) => {
+        char in counter ? counter[char]++ : counter[char] = 1
+        return counter
+    }, {})
+    let oddCounter = 0
+    let longestPalindrome = 0
+    for(let prop in charCount){
+        if(charCount[prop] %2 == 0){
+            longestPalindrome += charCount[prop]
+        }else{
+            if(oddCounter == 0){
+                longestPalindrome += charCount[prop]
+                oddCounter++
+            }else{
+                longestPalindrome += charCount[prop] - 1
+            }
+        }
+    }
+    return longestPalindrome
+};
