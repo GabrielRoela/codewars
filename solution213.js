c0cb51c929dc96cb58b2be28eaf9a1c26e5f7353
Gabriel Roela <gabrielbaltico24@gmail.com>
2022-12-24 16:56:02 -0300

    leetcode practice

---
+++ solution213.js
@@ -0,0 +1,39 @@
+// iven a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+var searchInsert = function(nums, target) {
+    if(nums.indexOf(target) === -1){
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] > target){
+                return i
+            }
+        }
+        return nums.length
+    }else{
+        return nums.indexOf(target)
+    }
+};
